d27ef7bfbdb459832381792ad382fa4603d6b26a
cexperto <57972985+cexperto@users.noreply.github.com>
2020-01-31 00:29:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -134,33 +134,7 @@ font-weight: bold;
   ?>
 <div></div>
 
-  <form class="px-4 py-3" action="enviarCorreo.php" method="POST">
-    <input type="text" value="" name="marcaU" id="marcaU" placeholder="marca" class="form-control" required>
-    <div style="width:550px;height: 7px;"></div>
-    <input type="text" value="" name="referenciaU" id="referenciaU" placeholder="referencia" class="form-control" required >
-    <div style="width:550px;height: 7px;"></div>
-    <input type="text" value="" name="modeloU" id="modeloU" placeholder="modelo" class="form-control" required>
-    <label class="form-check-label" for="exampleCheck1"></label>
-    <div style="width:550px;height: 7px;"></div>
-    <select name="soatU" id="soatU" class="form-control" required>
-        <option value="">vigencia de papeles</option>  
-        <option value="a">DE 0 A 1 MES y 29 DIAS</option>
-        <option value="b">DE 2 A 5 MESES Y 29 DIAS</option>
-        <option value="c">DE 6 A 10 MESES Y 29 DIAS</option>
-        <option value="d">DE 11 MESES A NUEVO</option>
-    </select>
-    <div></div>
-    <input type="submit" value="Enviar" id="enviarU" name="enviarU" class="form-control" style="
-   
-    background-color: #ffcd00;
-    font-weight: bold;
-    color: #213f99;
-    font-weight: bold;
-    ">
-    <div></div>
-   <div id=res></div>
-</form>
-
+  
 
 
 <!--                                                    /////////////////////////////////////////////////////////////////// -->
@@ -234,6 +208,21 @@ $(document).on('ready',function(){
     });
 });
 //
+//email
+	$(document).on('ready',function(){       
+    $('#soat').click(function(){
+        var soat = $(this).val();
+        console.log(modelo); 
+        if(soat){
+            document.getElementById("correo").removeAttribute("disabled");
+	    document.getElementById("telefono").removeAttribute("disabled");
+        }else{
+            document.getElementById("correo").setAttribute("disabled","");
+	    document.getElementById("telefono").setAttribute("disabled","");
+        }
+        
+    });
+});
 
 //
 //exampleCheck1
