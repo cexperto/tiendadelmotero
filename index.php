a40147ff9174337e2b8d5e06549e254ea197cc9b
cexperto <57972985+cexperto@users.noreply.github.com>
2020-02-04 02:49:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include('conexion.php');
 ?>
+<head>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
@@ -14,19 +15,30 @@ crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap" rel="stylesheet">
-
-
-	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
-
+<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
 <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
 <script type="text/javascript" src="https://cdn.navdmp.com/req?v=9&amp;id=a88fec8955ca4c3e451ed986e09%7C0&amp;acc=58464&amp;tit=Compramos%2520tu%2520carro%2520f%25E1cil%252C%2520r%25E1pido%2520y%2520seguro%2520%257C%2520Carro123.com&amp;upd=1&amp;h1=COMPRAMOS%2520TU%2520CARRO&amp;url=https%253A%2F%2Fwww.carro123.com%2F" async=""></script>
 
-<center>
-<div class="container">
-<div class="dropdown-menu"></div>
-<form id="precio" class="px-4 py-3" action="" method="POST">
+<body background="motosh.png" style="background-repeat: no-repeat;">
+<div class="container-fluid">
+  <br>
+  <div class="row">
+    <div class="col-sm-5 col-md-6" ><center><!--inica comoluna 1-->
+    <h3><p style="color:#fff";>
+    COMPRAMOS TU MOTO
+    </p></h3>
+
+    <!--termina columna-->
+    </div>
+
+
+    <!--columna 2-->
+    <div class="col-sm-5 col-sm-offset-2 col-md-6 col-md-offset-0"><!--inica comoluna 2-->
+    <center>
+    <div class="container">
+    <form id="precio" class="px-4 py-3" action="conocerPrecio.php" method="POST">
 <div class="form-group"></div>
-<center>
+
 <h3>Ingresa los datos de tu moto</h3>
   <div id="mm" class="borde-caja"> 
      <select name="marca" id="marca" class="form-control" onclick="myFunctionM()">
@@ -89,29 +101,29 @@ crossorigin="anonymous"></script>
 <input type="submit" value="QUIERO CONOCER EL PRECIO DE MI MOTO" id="enviar" name="enviar" class="form-control boton" 
 style="background-color:#ffcd00;color: #213f99;font-weight: bold;"><div></div>
 
-<div id="res" class="resultado"></div>
-<div class="nota"><br>
+<div id="res" class="resultado">
 <h4>
   nota: si tu moto no se encuentra en el listado o tienes
    alguna pregunta adicional diligencia el siguiente formulario 
    especificando marca y referencia de la moto, modelo y 
    vigencia de papeles y en menos de lo que canta un gallo 
    te responderemos</h4>
+
+</div>
+<div class="espacio"></div>
+
+<input type="submit" value="NO ENCUENTRO MI MOTO" id="encontrar" name="encontrar" class="form-control boton" 
+style="background-color:#ffcd00;color: #213f99;font-weight: bold;"><div></div>
+    <!--                                                                termina columna 2-->
+
+    </div> 
+
   </div>
-  </form>
-  <?php
-  //include('formEnviarCorreo.php');
-  ?>
-<div></div>
+</div>
 
-  
+</body>
 
 
-<!--                                                    /////////////////////////////////////////////////////////////////// -->
-
-<div class="modal-body">
-	        	<div id="cargapost"></div> <!-- campo para cargar resultado -->
-	        </div>
 <script type="text/javascript">
 // enviar formulario
     $(document).on('ready',function(){       
@@ -132,13 +144,14 @@ style="background-color:#ffcd00;color: #213f99;font-weight: bold;"><div></div>
 </script>
 
 <script>
+    /* 
 $(document).on("submit","#precio",function(e){
     e.preventDefault();
-    /* $("#myModal").modal(); */
-    $.post("precio.php",$("#precio").serialize(),function(res){
+     $("#myModal").modal(); 
+    $.post("conocerPrecio.php",$("#precio").serialize(),function(res){
         $("#res").html(res);
     });
-})
+}) */
 </script>
 <!-- selects -->
 <script>
