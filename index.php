0d646ac585db3b9dbe359047bfd20b731c542401
cexperto <57972985+cexperto@users.noreply.github.com>
2020-02-05 02:21:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ crossorigin="anonymous"></script>
 <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
 <script type="text/javascript" src="https://cdn.navdmp.com/req?v=9&amp;id=a88fec8955ca4c3e451ed986e09%7C0&amp;acc=58464&amp;tit=Compramos%2520tu%2520carro%2520f%25E1cil%252C%2520r%25E1pido%2520y%2520seguro%2520%257C%2520Carro123.com&amp;upd=1&amp;h1=COMPRAMOS%2520TU%2520CARRO&amp;url=https%253A%2F%2Fwww.carro123.com%2F" async=""></script>
 
-<body background="motosh.png" style="background-repeat: no-repeat;">
+<body background="img/e91a9b7c-62d3-4d99-b3d2-28d9631a352d.jpg" style="background-repeat: no-repeat;">
 <div class="container-fluid">
   <br>
   <div class="row">
@@ -35,7 +35,7 @@ crossorigin="anonymous"></script>
     <!--columna 2-->
     <div class="col-sm-5 col-sm-offset-2 col-md-6 col-md-offset-0"><!--inica comoluna 2-->
     <center>
-    <div class="container">
+    <div id="contenedor" class="container">
     <form id="precio" class="px-4 py-3" action="conocerPrecio.php" method="POST">
 <div class="form-group"></div>
 
@@ -101,19 +101,31 @@ crossorigin="anonymous"></script>
 <input type="submit" value="QUIERO CONOCER EL PRECIO DE MI MOTO" id="enviar" name="enviar" class="form-control boton" 
 style="background-color:#ffcd00;color: #213f99;font-weight: bold;"><div></div>
 
-<div id="res" class="resultado">
-<h4>
-  nota: si tu moto no se encuentra en el listado o tienes
+<div id="" class="resultado" ALIGN="justify" style="
+    padding-top: 5px;
+    padding-right: 20px;
+    padding-bottom: 5px;
+    padding-left: 20px;
+    font-family:Arial;
+    font-weigth:normal;
+
+"></form>
+<h5><font color="White">
+
+   Nota: <br>Si tu moto no se encuentra en el listado o tienes
    alguna pregunta adicional diligencia el siguiente formulario 
    especificando marca y referencia de la moto, modelo y 
    vigencia de papeles y en menos de lo que canta un gallo 
-   te responderemos</h4>
+   te responderemos</h5></font>
 
 </div>
 <div class="espacio"></div>
 
-<input type="submit" value="NO ENCUENTRO MI MOTO" id="encontrar" name="encontrar" class="form-control boton" 
-style="background-color:#ffcd00;color: #213f99;font-weight: bold;"><div></div>
+
+
+<div>
+
+</div>
     <!--                                                                termina columna 2-->
 
     </div> 
@@ -123,24 +135,9 @@ style="background-color:#ffcd00;color: #213f99;font-weight: bold;"><div></div>
 
 </body>
 
-
-<script>
-    
-$(document).on("submit","#precio",function(e){
-    e.preventDefault();
-     $("#myModal").modal(); 
-    $.post("conocerPrecio.php",$("#precio").serialize(),function(res){
-        $("#res").html(res);
-    });
-}) 
-</script>
-<!-- selects -->
-<script>
-   
-</script>
+<script src="permitir.js"></script>
 <script src="activarcampos.js"></script>
 <script src="activarcolores.js"></script>
-
 <script src="validaciones.js"></script>
 <script src="marca.js"></script>
 <script src="referencia.js"></script>
